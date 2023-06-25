a11faf5b1718a98685abd7e98c8ea394ee0ea615
Git-North <72702125+Git-North@users.noreply.github.com>
2023-06-25 05:15:41 +0300

    It's ready

--- forge-grab.bat
+++ forge-grab.bat
@@ -1,6 +1,5 @@
 
 @echo off
-setlocal enabledelayedexpansion
 
 set /p ver=
 set ftmp=forgehref.tmp
@@ -9,13 +8,9 @@ curl https://files.minecraftforge.net/net/minecraftforge/forge/index_%ver%.html 
 powershell -command (Get-Content -Path '.\%ftmp%3' -TotalCount 2)[-1] > %ftmp%2
 powershell -command iex .\script.ps1
 
-for /f "delims=" %%F in ('dir /s /b *^|.tmp#*') do (
-  del "%%F" /q
-)
-
-
-
 
 call %ftmp%.cmd
-echo curl %href% >> %ftmp%.cmd
+curl %href% -L -o forge-installer.jar
+
+del *.tmp?
 pause
